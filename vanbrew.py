a73bcbfa0764dc580c21ce3a86d9751e041737c3
Juanshep1 <Shephard_Juan@yahoo.com>
2026-06-28 11:21:42 -0500

    vanta 4.9: input + global hotkeys for game trainers

--- vanbrew.py
+++ vanbrew.py
@@ -58,7 +58,7 @@ def _u(path):
 #   bin:   {"name": "<cmd>", "kind": "python|vanta|exec|shell", "main": "<file>"}
 BUILTIN = {
     "vanta": {
-        "version": "4.8",
+        "version": "4.9",
         "summary": "The Vanta plain-English programming language & interpreter",
         "files": [{"source": _u("/packages/vanta/vanta.py"), "as": "vanta.py"}],
         "bin": [{"name": "vanta", "kind": "python", "main": "vanta.py"}],
